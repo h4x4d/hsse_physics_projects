dfb9ec1dfe6fd2397833253c2c1a9dfb6088a446
h4x4d <sidenkoleg@gmail.com>
2024-10-02 15:40:57 +0300

    add landing

--- objects/rocket.py
+++ objects/rocket.py
@@ -1,6 +1,8 @@
 import enum
-from vpython import vector, color, cone
+from vpython import vector, color, cone, diff_angle
 from math import sin, cos, radians, pi, sqrt, exp
+
+from vpython.no_notebook import stop_server
 
 from objects.ISS import ISS
 from objects.earth import Earth
@@ -13,6 +15,10 @@ class Status(enum.Enum):
     ORBIT = 4
     HOHMANN = 5
     RAISING_ISS = 6
+    LANDING = 7
+    ROTATE_ON_ORBIT = 8
+    BRAKING = 9
+    LANDED = 10
     NO_FUEL = 100
 
 
@@ -37,21 +43,24 @@ class Rocket:
             pos=self.pos,
             axis=axis,
             color=color.white,
-            radius=100000,
-            length=100000,
+            radius=100000,  # 25
+            length=100000,  # 50
             make_trail=True if trail_radius > 0 else False,
             trail_radius=trail_radius
         )
 
         self.acceleration = 0
-        self.fuel_mass = 6000
+        self.fuel_mass = 10000
 
         self.status = Status.TAKEOFF
 
         self.last_angle = 0.0
+        self.last_pos = 10**20
 
         self.timing = 0
         self.start_pos = 0
+
+        self.max_speed_now = 1000
 
         if camera:
             camera.follow(self.object)
@@ -192,10 +201,12 @@ class Rocket:
 
         self.timing += dt
 
-        if self.timing >= 100:
+        if 120 <= self.timing <= 10 ** 9:
             self.timing = -10 ** 20
             self.start_hohmann()
             self.status = Status.HOHMANN
+        elif 10 ** 9 + 150 <= self.timing:
+            self.status = Status.ROTATE_ON_ORBIT
 
     def update_hohmann(self, dt):
         self.update_on_orbit(dt)
@@ -206,8 +217,7 @@ class Rocket:
 
             second_speed = self.orbital_speed(self.pos.mag)
             need_vec = self.pos.cross(self.ORBIT_AXIS).hat
-
-            self.speed = need_vec * (second_speed+40)
+            self.speed = need_vec * (second_speed + 40)
         else:
             self.last_angle = self.pos.diff_angle(self.start_pos)
 
@@ -217,14 +227,19 @@ class Rocket:
         else:
             self.update_on_orbit(dt)
 
-        if (ISS.object.pos - self.pos).mag < 3000:
-
+        if (ISS.object.pos - self.pos).mag > self.last_pos:
+            print((ISS.object.pos - self.pos).mag)
+            need_vec = self.pos.cross(self.ORBIT_AXIS).hat
+            self.speed -= need_vec * 40
+            self.timing = 10 ** 9
             self.status = Status.ORBIT
+        else:
+            self.last_pos = (ISS.object.pos - self.pos).mag
 
     def raise_on_orbit_iss(self, speed, dt):
         need_vec = self.pos.cross(self.ORBIT_AXIS)
 
-        free_fall_acceleration = speed**2/self.pos.mag
+        free_fall_acceleration = speed ** 2 / self.pos.mag
         self.speed -= (self.pos.hat *
                        (free_fall_acceleration * dt))
 
@@ -234,9 +249,89 @@ class Rocket:
         self.object.rotate(self.object.axis.diff_angle(need_vec) * dt,
                            vector(sin(radians(51)), -cos(radians(51)), 0))
 
+    def update_rotate_on_orbit(self, dt):
+        need_vec = -self.pos.cross(self.ORBIT_AXIS)
+        diff_angle = need_vec.diff_angle(self.object.axis)
+        # print(diff_angle)
+        if diff_angle < self.last_angle:
+            self.object.rotate(max(0.0, min(dt * pi / 12, diff_angle)),
+                               vector(sin(radians(51)), -cos(radians(51)), 0))
+            self.last_angle = diff_angle
+        else:
+            self.status = Status.BRAKING
+
+        self.update_on_orbit(dt)
+
+    def update_braking(self, dt):
+        need_vec = self.pos.cross(self.ORBIT_AXIS).hat
+        if self.speed.dot(need_vec) > 0:
+            acc = 4 * Earth.FREE_FALL_ACCELERATION
+            self.speed -= need_vec * (acc)
+            spent_fuel = acc * self.mass / self.GAS_SPEED
+            self.fuel_mass -= spent_fuel * dt
+
+            if self.fuel_mass < 0:
+                self.status = Status.NO_FUEL
+        else:
+            self.status = Status.LANDING
+            return
+
+        free_fall_acceleration = (self.gravity_force() / self.mass)
+        self.speed -= (self.pos.hat *
+                       (free_fall_acceleration * dt))
+
+        self.pos += self.speed * dt
+        self.object.pos = self.pos
+
+    def update_landing(self, dt):
+        need_vec = self.pos
+        diff_angle = need_vec.diff_angle(self.object.axis)
+        if diff_angle > 0.01:
+            self.object.rotate(max(0.0, min(dt * pi / 12, diff_angle)) * dt,
+                               vector(sin(radians(51)), -cos(radians(51)), 0))
+
+        free_fall_acceleration = (self.gravity_force() / self.mass)
+        self.speed -= (self.pos.hat *
+                       (free_fall_acceleration * dt))
+
+        if (self.speed.diff_angle(self.pos) > pi / 2 and
+                self.speed.mag > self.max_speed_now):
+            acc = min(self.speed.mag - self.max_speed_now,
+                               4 * Earth.FREE_FALL_ACCELERATION) * dt
+            self.speed += (self.pos.hat * acc)
+            spent_fuel = acc * self.mass / self.GAS_SPEED
+            self.fuel_mass -= spent_fuel * dt
+
+
+        self.pos += self.speed * dt
+        self.object.pos = self.pos
+
+        if self.height > 100_000:
+            self.max_speed_now = 1000
+        elif 50_000 <= self.height < 100_000:
+            self.max_speed_now = 500
+        elif 10_000 <= self.height < 50_000:
+            self.max_speed_now = 250
+        elif 5_000 <= self.height < 10_000:
+            self.max_speed_now = 50
+        elif 500 <= self.height < 5_000:
+            self.max_speed_now = 25
+        elif 0 < self.height < 500:
+            self.max_speed_now = 20
+        else:
+            self.max_speed_now = 0
+
+        if self.height < 10:
+            self.status = Status.LANDED
+
     def no_fuel(self, *_, **__):
         print("NO FUEL")
-        raise ValueError("NO FUEL")
+        stop_server()
+
+    def update_landed(self, dt):
+        self.object.pos = self.pos.rotate(radians(1 / 240 * dt), axis=vector(0, 1, 0))
+        self.pos = self.object.pos
+        self.object.rotate(radians(1 / 240 * dt), axis=vector(0, 1, 0))
 
     def update(self, dt):
         statuses = {
@@ -246,6 +341,10 @@ class Rocket:
             Status.ORBIT: self.update_orbit,
             Status.HOHMANN: self.update_hohmann,
             Status.RAISING_ISS: self.update_iss_orbit,
-            Status.NO_FUEL: self.no_fuel
+            Status.BRAKING: self.update_braking,
+            Status.LANDING: self.update_landing,
+            Status.ROTATE_ON_ORBIT: self.update_rotate_on_orbit,
+            Status.NO_FUEL: self.no_fuel,
+            Status.LANDED: self.update_landed
         }
         statuses[self.status](dt)
--- plot/collect_logs.py
+++ plot/collect_logs.py
@@ -4,11 +4,11 @@ import time
 dt = [
     0.1, 1, 2, 5, 10, 50, 100
 ]
-MAX = 10000
+MAX = 50000
 
 if not os.path.exists("logs"):
     os.mkdir("logs")
 
 for i in dt:
-    os.system(f"python ../main.py --trail=10000 -r {int(MAX / i / 5)} -m {MAX} -l logs/logs{i}.csv -s {i} --stop")
+    os.system(f"python ../main.py --trail=10000 -r {int(MAX / i)} -m {MAX} -l logs/logs{i}.csv -s {i} --stop")
     time.sleep(5)
