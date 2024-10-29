bd55c50b048a3328766905ff7c1770941fff57c8
GorshkovIoann <ioann.gorshkoffo@mail.ru>
2024-10-29 20:58:42 +0300

    pr2

--- project_2/main.py
+++ project_2/main.py
@@ -1,11 +1,12 @@
 import pygame
+import math
 import numpy as np
 import random
 import matplotlib.pyplot as plt
 from fontTools.unicodedata import block
 
 h = 1  # spatial step width
-k = 1  # time step width
+k = 0.05  # time step width
 dimx = 200  # width of the simulation domain
 dimy = 200  # height of the simulation domain
 cellsize = 1  # display size of a cell in pixel
@@ -19,6 +20,18 @@ staticBlocks = [
     # [dimx // 2 - 40, dimy // 2 + 38, dimx // 2 + 40 , dimy // 2 + 40],
     # [0, 8, dimx-1, 9],
 ]
+# а давайте запускать не волну из одной точки а волну гаусовым распределением в 2д на 10 клеток
+sz = 10
+sigma = 1.2 # отклонение взято от балды
+xx, yy = np.meshgrid(range(-sz, sz), range(-sz, sz))
+gauss_peak = np.zeros((sz, sz))
+gauss_peak = 1 / (sigma*2*math.pi) * (math.sqrt(2*math.pi)) * np.exp(- 0.5 * ((xx**2+yy**2)/(sigma**2)))
+
+def put_gauss_peak(u, x : int, y : int, height):
+    w,h = gauss_peak.shape
+    w = int(w/2)
+    h = int(h/2)      
+    u[0:2, x-w:x+w, y-h:y+h] += height * gauss_peak
 
 
 def init_simulation():
@@ -28,9 +41,9 @@ def init_simulation():
     return u, alpha
 
 
-def update(u, alpha):
+        
+def update1(u, alpha):
     u[2], u[1] = u[1], u[0].copy()  # Avoid extra copying, reassign layers directly
-
     u[0, 1:dimx - 1, 1:dimy - 1] = (
             alpha[1:dimx - 1, 1:dimy - 1]
             * (
@@ -42,6 +55,28 @@ def update(u, alpha):
             )
     )
     u[0, 1:dimx - 1, 1:dimy - 1] += 2 * u[1, 1:dimx - 1, 1:dimy - 1] - u[2, 1:dimx - 1, 1:dimy - 1]
+    u[0, 1:dimx - 1, 1:dimy - 1] *= (0.995 ** k)
+    
+def update2(u, alpha):
+    "аналогичен update1 но использует дискретный оператор лапласа с точностью до O(x^4)"
+    u[2], u[1] = u[1], u[0].copy()  # Avoid extra copying, reassign layers directly
+    
+    u[0, 2:dimx-2, 2:dimy-2]  = alpha[2:dimx-2, 2:dimy-2]\
+                                    * ( -  1 * u[1, 2:dimx-2, 0:dimy-4]
+                                        + 16 * u[1, 2:dimx-2, 1:dimy-3]                                       
+
+                                        -  1 * u[1, 0:dimx-4, 2:dimy-2]  
+                                        + 16 * u[1, 1:dimx-3, 2:dimy-2]
+                                        - 60 * u[1, 2:dimx-2, 2:dimy-2]  
+                                        + 16 * u[1, 3:dimx-1, 2:dimy-2] 
+                                        -  1 * u[1, 4:dimx,   2:dimy-2]
+
+                                        + 16 * u[1, 2:dimx-2, 3:dimy-1]                                         
+                                        - 1  * u[1, 2:dimx-2, 4:dimy]    
+                                        ) / 12 \
+                                    + 2*u[1, 2:dimx-2, 2:dimy-2] \
+                                    -   u[2, 2:dimx-2, 2:dimy-2] 
+    
     u[0, 1:dimx - 1, 1:dimy - 1] *= (0.995 ** k)
 
 
@@ -63,7 +98,7 @@ def place_raindrops(u):
     if random.random() < 0.0002:
         x = random.randrange(5, dimx - 5)
         y = random.randrange(5, dimy - 5)
-        u[0, x - 2:x + 2, y - 2:y + 2] = 120
+        put_gauss_peak(u, x, y, 120)
 
 
 def center_wave(u, x, y, v):
@@ -90,12 +125,14 @@ def main():
     u, alpha = init_simulation()
     pixeldata = np.zeros((dimx, dimy, 3), dtype=np.uint8)
 
-    center_wave(u, dimx // 2, dimy // 2, 10000)
+    center_wave(u, dimx // 2, dimy // 2, 100) 
+
+    # put_gauss_peak(u, int(dimx/2), int(dimy/2), 3000)
     clock = pygame.time.Clock()
 
     x_cords = [0]
     y_cords = [0]
-    dt = 1
+    dt = 0.05
     max_time = 600
 
     while dt < max_time:
@@ -105,7 +142,7 @@ def main():
                 return
 
         # place_raindrops(u)  # Uncomment if you want random raindrops
-        update(u, alpha)
+        update1(u, alpha)
         add_static(u, staticBlocks)
 
         x_cords.append(dt)
